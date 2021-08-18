f44da307e099ff744935a483c61d65a9ec03e832
Eiffla <63313300+Eiffla@users.noreply.github.com>
2021-08-18 19:12:12 +0200

    Added slider with loop

--- resources/views/zakwaterowanie/zakwaterowanie.blade.php
+++ resources/views/zakwaterowanie/zakwaterowanie.blade.php
@@ -1,5 +1,14 @@
 @extends('template')
 
 @section('content')
-
+<section>
+    <div class="sl-accommodation" style = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)); background-image: url({{ URL::asset('img/slider.jpg') }});">
+      <div class="dark">
+        <div class="container">
+          <h1 class = "header-acc justify-content-center"><span style= "color:#2980b9;">Znajdź</span> zakwaterowanie</h1>
+            <p class = "slider">Przeszukaj naszą bazę mieszkań i pokoi w całej Polsce.</p>
+        </div>
+      </div>
+    </div>
+      </section>
 @endsection
