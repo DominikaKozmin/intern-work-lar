8b7b0c659ca2fb4416ad58d6207f831441561dc1
CichockiMateusz <86666788+CichockiMateusz@users.noreply.github.com>
2021-09-06 22:51:07 +0200

    Revert "Merge branch 'main' of https://github.com/Eiffla/intern-work-lar into main"
    This reverts commit 225efa21e01ac14a967e0f92fe903d487782d771, reversing
    changes made to c0d1beb39823e19bcad2dfa7d76527ef9da7855e.

--- app/Http/Controllers/OffersController.php
+++ app/Http/Controllers/OffersController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class OffersController extends Controller
 {
     public function index() {
-        return view('offers.offers');
+        echo "Witaj świecie!";
     }
 }
